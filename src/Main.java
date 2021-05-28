c9c1612153a08b939a8d6c5fe4c8722b1cc14b21
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-05-28 14:21:38 +0300

    Cleanup

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BodyMassIndex service = new BodyMassIndex();
+        double height = 173; // Рост в см
+        double weight = 60; // Вес в кг
+        double bodyMassIndex = service.calculate(height, weight);
+        String formattedBMI = String.format("%.2f", bodyMassIndex);
+        System.out.println("Индекс массы тела: " + formattedBMI);
+    }
+}
